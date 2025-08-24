a542059454ba14acd1e40e1ca389613c76ef4508
antohnycastilloastudillo-bit <antohnycastilloastudillo@gmail.com>
2025-08-23 20:33:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Generar QR con tu link en GitHub Pages
 var qrcode = new QRCode(document.getElementById("qrcode"), {
-  text: "https://antohnycastilloastudillo-bit.github.io.", // Reemplaza con tu link
+  text: "https://antohnycastilloastudillo-bit.github.io/menu-qr/",
   width: 200,
   height: 200
 });
